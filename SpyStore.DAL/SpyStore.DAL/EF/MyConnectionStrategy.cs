220b891a588eb2cc10a3248b372d5a81cb8621ed
Maxime Daniou <maxime.daniou@outlook.com>
2018-01-09 22:07:07 +0100

    configuring files

---
+++ SpyStore.DAL/SpyStore.DAL/EF/MyConnectionStrategy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+
+namespace SpyStore.DAL.EF
+{
+    public class MyExecutionStrategy : ExecutionStrategy
+    {
+        public MyExecutionStrategy(DbContext context) :
+            this(context, DefaultMaxRetryCount, DefaultMaxDelay)
+        {
+        }
+
+        public MyExecutionStrategy(DbContext context, int maxRetryCount, TimeSpan maxRetryDelay) :
+            base(context, maxRetryCount, maxRetryDelay)
+        {
+        }
+
+        public MyExecutionStrategy(ExecutionStrategyDependencies dependencies) :
+            this(dependencies, DefaultMaxRetryCount, DefaultMaxDelay)
+        {
+        }
+
+        public MyExecutionStrategy(ExecutionStrategyDependencies dependencies, int maxRetryCount, TimeSpan maxRetryDelay) :
+            base(dependencies, maxRetryCount, maxRetryDelay)
+        {
+        }
+
+        protected override bool ShouldRetryOn(Exception exception)
+        {
+            return true;
+        }
+    }
+}
---
+++ SpyStore.DAL/SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SpyStore.Models.Entities;
+
+namespace SpyStore.DAL.EF
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+        }
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSet=True;", options => options.EnableRetryOnFailure());
+            }
+        }
+    }
+}
