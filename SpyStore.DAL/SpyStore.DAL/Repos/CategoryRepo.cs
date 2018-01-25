9290cc9a0d3f82228c8c6d32bb7a48b97dd9bd0e
Maxime Daniou <maxime.daniou@outlook.com>
2018-01-25 19:14:40 +0100

    CHAP2_P063-82

--- SpyStore.DAL/SpyStore.DAL/Repos/CategoryRepo.cs
+++ SpyStore.DAL/SpyStore.DAL/Repos/CategoryRepo.cs
@@ -3,11 +3,12 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using SpyStore.DAL.EF;
 using SpyStore.DAL.Repos.Base;
+using SpyStore.DAL.Repos.Interfaces;
 using SpyStore.Models.Entities;
 
 namespace SpyStore.DAL.Repos
 {
-    public class CategoryRepo : RepoBase<Category>
+    public class CategoryRepo : RepoBase<Category>, ICategoryRepo
     {
         public CategoryRepo(DbContextOptions<StoreContext> options) : base(options)
         {
@@ -20,5 +21,12 @@ namespace SpyStore.DAL.Repos
 
         public override IEnumerable<Category> GetRange(int skip, int take)
             => GetRange(Table.OrderBy(x => x.CategoryName), skip, take);
+
+        public Category GetOneWithProducts(int? id)
+            => Table.Include(x => x.Products).FirstOrDefault(x => x.Id == id);
+
+        public IEnumerable<Category> GetAllWithProducts()
+            => Table.Include(x => x.Products);
+
     }
 }
