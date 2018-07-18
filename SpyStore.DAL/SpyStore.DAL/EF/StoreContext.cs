34abf212b205146ddbd927f66276283893163aba
mdaniou <md@mxbzh.com>
2018-07-18 15:57:32 +0200

    CHAP 3 : commit progression

--- SpyStore.DAL/SpyStore.DAL/EF/StoreContext.cs
+++ SpyStore.DAL/SpyStore.DAL/EF/StoreContext.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using SpyStore.Models.Entities;
 
 namespace SpyStore.DAL.EF
@@ -31,7 +28,8 @@ namespace SpyStore.DAL.EF
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer(
-            @"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSets=true;");
+                    @"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSets=true;"
+                    , options => options.ExecutionStrategy(c=> new MyExecutionStrategy(c)));
             }
             //if (!optionsBuilder.IsConfigured)
             //{
---
+++ SpyStore.Service/SpyStore.Service/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SpyStore.DAL.Initializers;
+
+namespace SpyStore.Service
+{
+    public class Startup
+    {
+        private IHostingEnvironment _env;
+        public Startup(IHostingEnvironment env)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+            _env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+                loggerFactory.AddDebug();
+            }
+
+            if (env.IsDevelopment())
+            {
+                using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    StoreDataInitializer.InitializeData(app.ApplicationServices);
+                }
+            }
+            app.UseCors("AllowAll");  // has to go before UseMvc
+
+            app.UseMvc();
+        }
+    }
+}
